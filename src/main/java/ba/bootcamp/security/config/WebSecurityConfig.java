ccb8221a95631557cb69b5d0d49a141526953c6c
ljubomir.cakarevic <ljubomir.cakarevic@gmail.com>
2020-12-29 23:42:53 +0100

    OpenAPI 3.0 dependency added

--- src/main/java/ba/bootcamp/security/config/WebSecurityConfig.java
+++ src/main/java/ba/bootcamp/security/config/WebSecurityConfig.java
@@ -56,12 +56,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
-			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-			.antMatchers("/api/test/**").permitAll()
-			.antMatchers("/v2/api-docs/", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
-			.anyRequest().authenticated();
+		.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+		.antMatchers("/api/test/**").permitAll()
+		.anyRequest().permitAll();
+		//.authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
