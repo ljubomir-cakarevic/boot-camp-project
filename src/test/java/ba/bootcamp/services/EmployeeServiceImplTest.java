953dcf0c53362465ca416cc7af0f609a699c8c38
ljubomir.cakarevic <ljubomir.cakarevic@gmail.com>
2021-02-05 17:27:53 +0100

    EmployeeService test

--- src/test/java/ba/bootcamp/services/EmployeeServiceImplTest.java
+++ src/test/java/ba/bootcamp/services/EmployeeServiceImplTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -44,6 +46,36 @@ class EmployeeServiceImplTest {
 		assertNotNull(employeeDto);
 		assertEquals("Milos", employeeDto.getFirstName());
 		assertEquals("Ilic", employeeDto.getLastName());
+	}
+	
+	@Test
+	void testGetAllEmployees() {
+		
+		List<Employee> data = new ArrayList<Employee>();
+		
+		data.add(new Employee("Milan", "Peric", 34, "milan@test.com", "Developer"));
+		data.add(new Employee("Dejan", "Simic", 47, "dejan@test.com", "Manager"));
+		data.add(new Employee("Slavko", "Jovic", 32, "slavko@test.com", "Developer"));
+		
+		when(employeeRepo.findAll()).thenReturn(data);
+		
+		List<EmployeeDto> employeesListDto = employeeService.getAllEmployees();
+		assertEquals(3, employeesListDto.size());
+	
+	}
+	
+	@Test
+	void testGetEmployeeByEmail() {
+		
+		Employee employee = new Employee("Marko", "Panic", 36, "test@test.com", "Manager");
+		String email = "test@test.com";
+		
+		when(employeeRepo.findByEmail(email)).thenReturn(employee);
+		
+		EmployeeDto employeeDto = employeeService.getEmployeeByEmail(email);
+		
+		assertEquals(employee.getPosition(), employeeDto.getPosition());
+		
 	}
 
 }
